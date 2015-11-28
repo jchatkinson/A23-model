49f007c0fd09430eb77539f0345d41d24dda4552
jchatkinson <jchatkinson@gmail.com>
2015-11-28 13:46:57 -0800

    wall forces are now plotted

--- ReadFiles.m
+++ ReadFiles.m
@@ -5,27 +5,26 @@ close all; clc; clear all;
 ModelName = '72x72-RSA-WallTotal.xlsx';
 WallPier = 'WallTotal';
 
-
 % Story data
 [StoryLabels,~,Elevations] = textread('StoryInformation.txt','%s%f%f');
 
 [~,~,SpandrelForces]= xlsread(ModelName,'Spandrel Forces');
-[~,~,PierForces] = xlsread(ModelName,'Pier Forces');
+[~,~,PierTable] = xlsread(ModelName,'Pier Forces');
 [~,~,StoryDrifts] = xlsread(ModelName,'Story Drifts');
 [~,~,StoryDisp] = xlsread(ModelName,'Story Max Avg Displacements');
 
 
 % Wall Forces
-PierStoryLabels = PierForces(4:end,1);
-PierName = PierForces(4:end,2);
-PierLoadCombo = PierForces(4:end,3);
-PierForces = PierForces(4:end,6:12);
+PierStoryLabels = PierTable(4:end,1);
+PierName = PierTable(4:end,2);
+PierLoadCombo = PierTable(4:end,3);
+PierForces = PierTable(4:end,6:12);
 
 % Coupling beam story
 SpandrelStoryLabels = SpandrelForces(4:end,1);
-SpandralName = SpandrelForces(4:end,2);
+SpandrelName = SpandrelForces(4:end,2);
 SpandrelLoadCombo = SpandrelForces(4:end,3);
-SpandralForces = SpandrelForces(4:end,8);
+SpandrelForces = SpandrelForces(4:end,8);
 
 % Story drifts
 DriftsStoryLabels = StoryDrifts(4:end,1);
@@ -37,3 +36,74 @@ DispStoryLabels = StoryDisp(4:end,1);
 DispLoadCombo = StoryDisp(4:end,2);
 DispStoryDrifts = StoryDisp(4:end,6:7);
 
+
+
+%% Example - filter data by loadCombo
+
+% myCombo = 'RSAx'; % 'RSAx' 'RSAx-nT' 'RSAx-pT' 'RSAy' 'RSAy-nT' 'RSAy-pT' 'AutoWind1' 'AutoWind2'
+% myPier = 'WallTotal';
+% index = find(strcmp(PierName,myPier) & strcmp(PierLoadCombo,myCombo));
+% Lvls = PierStoryLabels(index);
+% Forces = PierForces(index,:);
+% Data = [Lvls,num2cell(mergeVectors(Elevations(1:end-1),Elevations(2:end))),Forces];
+
+%% Plot Wall Forces (for each pier,combo)
+
+piers = {'WallTotal'};
+plots={'P','V2','V3','M2','M3'};
+col=[4,5,6,8,9];
+% combos={'RSAx' 'RSAx-nT' 'RSAx-pT' 'RSAy' 'RSAy-nT' 'RSAy-pT' 'AutoWind1' 'AutoWind2'};
+combos={'RSAx' 'RSAx-nT' 'RSAy' 'RSAy-nT' 'RSAy-pT' };
+% linetypes={'r','r','r','m','m','m','b','b'};
+linetypes={'r','r','m','m','m','b','b'};
+
+
+for aa = 1:length(piers)
+  figure; hold on;
+  
+  for bb = 1:length(combos)
+    %get the data
+    index = find(strcmp(PierName,piers{aa}) & strcmp(PierLoadCombo,combos{bb}));
+    Lvls = PierStoryLabels(index);
+    Forces = PierForces(index,:);
+    Data = [Lvls,num2cell(mergeVectors(Elevations(1:end-1),Elevations(2:end))),Forces];
+    for cc = 1:length(plots)
+      subplot(1,length(plots),cc); hold on;
+      plot(cell2mat(Data(:,col(cc))),cell2mat(Data(:,2)),linetypes{bb});
+      xlabel(plots{cc});
+    end
+  end
+  suptitle(piers{aa});
+  hold off
+end
+
+
+%% Plot Coupling beam forces
+
+cbnames = {'CB-NE','CB-NW','CB-SE','CB-SW'};
+plots={'P','V2','V3','M2','M3'};
+col=[4,5,6,8,9];
+% combos={'RSAx' 'RSAx-nT' 'RSAx-pT' 'RSAy' 'RSAy-nT' 'RSAy-pT' 'AutoWind1' 'AutoWind2'};
+combos={'RSAx' 'RSAx-nT' 'RSAy' 'RSAy-nT' 'RSAy-pT' };
+% linetypes={'r','r','r','m','m','m','b','b'};
+linetypes={'r','r','m','m','m','b','b'};
+
+
+for aa = 1:length(piers)
+  figure; hold on;
+  
+  for bb = 1:length(combos)
+    %get the data
+    index = find(strcmp(PierName,piers{aa}) & strcmp(PierLoadCombo,combos{bb}));
+    Lvls = PierStoryLabels(index);
+    Forces = PierForces(index,:);
+    Data = [Lvls,num2cell(mergeVectors(Elevations(1:end-1),Elevations(2:end))),Forces];
+    for cc = 1:length(plots)
+      subplot(1,length(plots),cc); hold on;
+      plot(cell2mat(Data(:,col(cc))),cell2mat(Data(:,2)),linetypes{bb});
+      xlabel(plots{cc});
+    end
+  end
+  suptitle(piers{aa});
+  hold off
+end
